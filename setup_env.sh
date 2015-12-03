8d688ee699ed9fa3c19241478a3ea113ea0fabba
daviddunne <daviddunnepc@gmail.com>
2015-12-03 16:32:32 +0000

    edited shell files

--- setup_env.sh
+++ setup_env.sh
@@ -4,10 +4,6 @@ echo "Activating virtual environment"
 source venv/bin/activate
 echo "virtual environment activated"
 
-echo "Changing directory"
-cd Flu_Tracker
-echo "Directory changed"
-
 echo "Downloading requirements"
 pip install -r requirements.txt
 echo "Requirements installed"
