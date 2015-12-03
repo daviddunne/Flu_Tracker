3b86b1ad8a6a50afb19a8f2639549600fd6785f8
daviddunne <daviddunnepc@gmail.com>
2015-12-03 16:26:17 +0000

    split shell script into 2 for environment setup and run test commands

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
-
-echo "Activating virtual environment"
-source venv/bin/activate
-echo "virtual environment activated"
-
-echo "Changing directory"
-cd Flu_Tracker
-echo "Directory changed"
-
-echo "Downloading requirements"
-pip install -r requirements.txt
-echo "Requirements installed"
-
 echo "Starting tests now"
 python3 -m discover
---
+++ setup_env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Activating virtual environment"
+source venv/bin/activate
+echo "virtual environment activated"
+
+echo "Changing directory"
+cd Flu_Tracker
+echo "Directory changed"
+
+echo "Downloading requirements"
+pip install -r requirements.txt
+echo "Requirements installed"
