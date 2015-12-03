2352b08efd50d7402d58cd6f87dfa94c7c907f27
daviddunne <daviddunnepc@gmail.com>
2015-12-03 15:03:20 +0000

    edited requirements file and changed script for running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 
-echo "Starting tests now"
+echo "Activating virtual environment"
 source venv/bin/activate
-cd Flu_Tracker
+echo "virtual environment activated"
 
+echo "Changing directory"
+cd Flu_Tracker
+echo "Directory changed"
+
+echo "Starting tests now"
 python3 -m discover
